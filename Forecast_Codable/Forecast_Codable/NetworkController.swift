03e5cde5dbee8c21509c7711285083420b7b7783
Matheus Oliveira <matheus.oliveira254@gmail.com>
2022-10-10 15:00:22 -0600

    finalized the fetch func

--- Forecast_Codable/Forecast_Codable/NetworkController.swift
+++ Forecast_Codable/Forecast_Codable/NetworkController.swift
@@ -12,7 +12,7 @@ class NetworkController {
     
     //MARK: - CRUD
     static func fetchDays(completion: @escaping (TopLevelDictionary?) -> Void) {
-        guard let baseURL = URL(string: baseURLString) else {return}
+        guard let baseURL = URL(string: baseURLString) else {completion(nil); return}
         var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
         urlComponents?.path = "v2.0/forecast/daily"
         var apiQuery = URLQueryItem(name: "key", value: "dd42671717fd414aa38bc749daace27b")
@@ -22,5 +22,21 @@ class NetworkController {
         urlComponents?.queryItems = [apiQuery, cityQuery, unitsQuery]
         guard let finalURL = urlComponents?.url else {return}
         print(finalURL)
+        
+        URLSession.shared.dataTask(with: finalURL) { dayData, _, error in
+            if let error {
+                print("There was a problem fetching the data. The url is \(finalURL), the error is \(error.localizedDescription)")
+                completion(nil)
+                return
+            }
+            
+            guard let data = dayData else {print("There was an error receiving the data!"); completion(nil); return}
+            do {
+                let topLevelDictionary = try JSONDecoder().decode(TopLevelDictionary.self, from: data)
+            } catch {
+                print("Error in DO/TRY/CATCH: \(error.localizedDescription)")
+                completion(nil)
+            }
+        }.resume()
     }
 }
